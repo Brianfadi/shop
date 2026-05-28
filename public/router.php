13140c93f072824838961cd0a53351fcaece8b8f
Brianfadi <brianfadi20@gmail.com>
2026-05-28 10:54:54 +0300

    fix: add router.php to serve static assets correctly with php built-in server

---
+++ public/router.php
@@ -0,0 +1,11 @@
+<?php
+
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+// Serve static files directly
+if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
+    return false;
+}
+
+// Otherwise route through Laravel
+require_once __DIR__ . '/index.php';
